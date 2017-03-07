3af397464bc59e676d62468c5441a466bc70d394
Cassiano Melo <cassiano91@gmail.com>
2017-03-07 16:42:32 -0300

    Arquivos Iniciais

---
+++ index.php
@@ -0,0 +1,19 @@
+ <?php
+	$path = getcwd();
+	$diretorio = dir($path);
+
+	$server   = $_SERVER['SERVER_NAME'];
+	$endereco = $_SERVER ['REQUEST_URI'];
+	$url      = "http://" . $server . $endereco;
+	echo $url . "<br /><br />";
+
+	echo "Arquivos '<strong>".$path."</strong>':<br /><br />";
+
+	while($arquivo = $diretorio -> read()){
+		if (strpos($arquivo, "html")) {
+			echo "<a href='" . $url . $arquivo . "'>" . $arquivo . "</a><br />";
+		}
+	}
+
+	$diretorio -> close();
+?>
